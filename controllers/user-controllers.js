b88e3e29017f71a2f8da5656feb53776618d9069
Hatim Khuzema <hatimk51@gmail.com>
2022-08-15 18:41:36 +0500

    Hashed Password

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
@@ -13,7 +14,7 @@ const getUsers = async (req, res, next) => {
     return next(new HttpError("Getting users failed."));
   }
 
-  res.json({ users: users.map(user => user.toObject( { getters: true } )) });
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
@@ -38,12 +39,19 @@ const signup = async (req, res, next) => {
     return next(new HttpError("User already exist, login instead", 422));
   }
 
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    console.log(err);
+    return next(new HttpError("Signing up failed. Please try agin.", 500));
+  }
+
   const createdUser = new User({
     name,
     email,
-    password,
+    password: hashedPassword,
     image: req.file.path,
-    places: []
+    places: [],
   });
 
   try {
@@ -66,13 +74,33 @@ const login = async (req, res, next) => {
     console.log(err);
     return next(new HttpError("Login failed. Please try agin.", 500));
   }
-  if (!user || user.password !== password) {
+  if (!user) {
     return next(
       new HttpError("No user found having provided email and password.", 401)
     );
   }
 
-  res.status(200).json({ message: "Login successful.", user: user.toObject({getters: true}) });
+  let isValidPassword = false;
+
+  try {
+    isValidPassword = await brycpt.compare(password, user.password);
+  }
+  catch (err) {
+    return next(
+      new HttpError("Login failed. Please try agin.", 500)
+    );
+  }
+
+  if(!isValidPassword) {
+    return next(
+      new HttpError("No user found having provided email and password.", 401)
+    );
+  }
+
+  res.status(200).json({
+    message: "Login successful.",
+    user: user.toObject({ getters: true }),
+  });
 };
 
 exports.getUsers = getUsers;
